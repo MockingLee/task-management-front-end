d54911bfbda7ff46358fb5fbb51cd85995535a79
MockingLee <38075220+MockingLee@users.noreply.github.com>
2019-05-16 17:01:47 +0800

    login init

--- config/index.js
+++ config/index.js
@@ -10,7 +10,12 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+			'/api': {
+				target: 'http://9.112.92.151:8080',
+				changeOrigin: true
+			}
+		},
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
