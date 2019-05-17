8cd15efffe600c2c6f86b3375e9f52533ca3a38b
MockingLee <38075220+MockingLee@users.noreply.github.com>
2019-05-17 15:37:23 +0800

    fix manage.vue

--- src/pages/manage.vue
+++ src/pages/manage.vue
@@ -36,6 +36,71 @@
         <!--页眉-->
         <el-header class="header">
           <el-row>
+            <el-col :span="18"
+                    class="header-title">
+              <span v-if="collapsed"
+                    class="system-name">{{systemName}}</span>
+            </el-col>
+            <el-col :span="6"
+                    :offset="18">
+              <span v-on:click="home">
+                <i class="fa fa-home"
+                   aria-hidden="true"></i>
+              </span>
+              &emsp;
+              <span v-on:click="login">
+                <i class="fa fa-sign-out"></i>
+              </span>
+            </el-col>
+          </el-row>
+        </el-header>
+        <!--中间-->
+        <el-main class="main">
+          <hr>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+
+  </section>
+</template>
+  <section>
+    <el-container class="container">
+      <!--左边-->
+      <el-aside :width="collapsed? '65px' : '200px' ">
+        <el-container>
+          <el-header>
+            <span class="menu-button"
+                  v-if="collapsed"
+                  @click.prevent="collapsed=!collapsed">
+              <i class="fa fa-align-justify"></i>
+            </span>
+            <span v-else
+                  class="system-name">{{systemName}}</span>
+          </el-header>
+          <el-main>
+            <el-menu :collapse="collapsed">
+              <el-menu-item-group>
+                <router-link to='/add'>
+                  <center>
+                    <el-menu-item index="1-1">录入用户</el-menu-item>
+                  </center>
+                </router-link>
+                <router-link to='/index'>
+                  <center>
+                    <el-menu-item index="1-2">查询用户</el-menu-item>
+                  </center>
+                </router-link>
+              </el-menu-item-group>
+            </el-menu>
+          </el-main>
+        </el-container>
+      </el-aside>
+      <!--内容-->
+      <el-container>
+        <!--页眉-->
+        <el-header class="header">
+          <el-row>
 
             <el-col :span="18"
                     class="header-title">
@@ -65,31 +130,33 @@
         <!--中间-->
         <el-main class="main">
           <hr>
+        </el-main>
+
+      </el-container>
+
+      <!--内容-->
+      <el-container>
+        <!--页眉-->
+        <el-header class="header">
+          <el-row>
+
+            <el-col :span="18"
+                    class="header-title">
+
+              <span v-if="collapsed"
+                    class="system-name">{{systemName}}</span>
+            </el-col>
+            <el-col :span="15"
+                    :offset="15"><span class="system-name">{{userName}}</span><span v-on:click="logout"><i class="fa fa-sign-out"></i></span></el-col>
+          </el-row>
+        </el-header>
+        <!--中间-->
+        <el-main class="main">
+          <hr>
           <router-view></router-view>
         </el-main>
 
       </el-container>
-    </el-aside>
-    <!--内容-->
-    <el-container>
-      <!--页眉-->
-      <el-header class="header">
-        <el-row>
-          
-          <el-col :span="18" class="header-title">       
-            
-               <span v-if="collapsed" class="system-name">{{systemName}}</span>            
-          </el-col>
-          <el-col :span="15" :offset="15"><span class="system-name">{{userName}}</span><span v-on:click="logout"><i class="fa fa-sign-out"></i></span></el-col>
-        </el-row>
-      </el-header>
-      <!--中间-->
-      <el-main class="main">
-        <hr>
-        <router-view></router-view>
-      </el-main> 
-      </el-main>
-    </el-container>
 
   </section>
 </template>
@@ -99,7 +166,7 @@ let data = () => {
   return {
     collapsed: false,
     systemName: '任务管理',
-    userName: JSON.parse(localStorage.token).username , 
+    userName: JSON.parse(localStorage.token).username,
     filters: {},
     rows: []
 
@@ -140,7 +207,7 @@ export default {
     handleDelete,
     //获取分页
     getRows,
-    logout() {
+    logout () {
       console.log(localStorage)
       localStorage.removeItem('token')
       this.$router.push("/login");
